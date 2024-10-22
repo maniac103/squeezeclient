b1dbebb131b79fd2a15b88e7b5fc00f72a47383f
Danny Baumann <dannybaumann@web.de>
2024-10-22 10:11:23 +0200

    Bump version and add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "de.maniac103.squeezeclient"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         vectorDrawables {
             useSupportLibrary = true

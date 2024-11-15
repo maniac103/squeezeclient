c409532d74ab366b9acd41b7769cf636dceee099
Danny Baumann <dannybaumann@web.de>
2024-11-15 10:48:43 +0100

    Properly extend image viewer toolbar scrim under status bar

--- app/src/main/java/de/maniac103/squeezeclient/ui/slideshow/ImageViewActivity.kt
+++ app/src/main/java/de/maniac103/squeezeclient/ui/slideshow/ImageViewActivity.kt
@@ -22,7 +22,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.webkit.MimeTypeMap
-import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -47,9 +46,7 @@ class ImageViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(0xA0000000.toInt())
-        )
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityImageBinding.inflate(layoutInflater)
         setContentView(binding.root)

maniac103/squeezeclient

commit e51db9a611de37345cf34f095d14db33ea8717e6

Improve artwork and image corner rounding
Do rounding solely via ShapeableImageView where possible, make corner
radii consistent and fix them for non-square images.

app/src/main/java/de/maniac103/squeezeclient/ui/common/SlimBrowseItemListViewHolder.kt
@@ -26,6 +26,7 @@ import android.widget.TextView
 import androidx.core.view.forEach
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import de.maniac103.squeezeclient.R
 import de.maniac103.squeezeclient.extfuncs.loadArtworkOrPlaceholder
@@ -68,7 +69,7 @@ class SlimBrowseItemListViewHolder(
 
     data class ItemBinding(
         val root: View,
-        val icon: ImageView,
+        val icon: ShapeableImageView,
         val title: TextView,
         val subText: TextView,
         val contextContainer: ViewGroup,

app/src/main/java/de/maniac103/squeezeclient/extfuncs/ContextExtensions.kt
@@ -59,5 +59,4 @@ suspend fun Context.loadArtwork(item: ArtworkItem?, size: Int): Drawable? {
     return imageLoader.execute(request)
         .image
         ?.asDrawable(resources)
-        ?.withRoundedCorners(this)
 }

app/src/main/java/de/maniac103/squeezeclient/extfuncs/ViewExtensions.kt
@@ -29,6 +29,8 @@ import androidx.core.view.updatePadding
 import coil3.load
 import coil3.request.ImageRequest
 import coil3.request.placeholder
+import coil3.target.ImageViewTarget
+import com.google.android.material.imageview.ShapeableImageView
 import de.maniac103.squeezeclient.R
 import de.maniac103.squeezeclient.model.ArtworkItem
 import de.maniac103.squeezeclient.model.SlideshowImage
@@ -40,14 +42,16 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 fun View.animateScale(scale: Float, duration: Duration) =
     animate().scaleX(scale).scaleY(scale).setDuration(duration.inWholeMilliseconds)
 
-fun ImageView.loadArtwork(item: ArtworkItem?, builder: ImageRequest.Builder.() -> Unit = {}) =
-    load(item?.extractIconUrl(context)) {
-        addServerCredentialsIfNeeded(context)
-        target(RoundedCornerImageViewTarget(this@loadArtwork))
-        builder()
-    }
+fun ShapeableImageView.loadArtwork(
+    item: ArtworkItem?,
+    builder: ImageRequest.Builder.() -> Unit = {}
+) = load(item?.extractIconUrl(context)) {
+    addServerCredentialsIfNeeded(context)
+    target(ImageViewTarget(this@loadArtwork))
+    builder()
+}
 
-fun ImageView.loadArtworkOrPlaceholder(
+fun ShapeableImageView.loadArtworkOrPlaceholder(
     item: ArtworkItem?,
     builder: ImageRequest.Builder.() -> Unit = {}
 ) = loadArtwork(item) {
@@ -57,16 +61,13 @@ fun ImageView.loadArtworkOrPlaceholder(
 
 fun ImageView.loadSlideshowImage(
     item: SlideshowImage,
-    roundCorners: Boolean,
     builder: ImageRequest.Builder.() -> Unit = {}
 ) = item.imageUrl.let { url ->
     val baseUrl = context.prefs.serverConfig?.url
     val absoluteUrl = baseUrl?.resolve(url)?.toString() ?: url
     load(absoluteUrl) {
         addServerCredentialsIfNeeded(context)
-        if (roundCorners) {
-            target(RoundedCornerImageViewTarget(this@loadSlideshowImage))
-        }
+        target(ImageViewTarget(this@loadSlideshowImage))
         builder()
     }
 }

app/src/main/java/de/maniac103/squeezeclient/ui/MainContentContainerFragment.kt
@@ -44,6 +44,7 @@ import de.maniac103.squeezeclient.extfuncs.imageCacheContains
 import de.maniac103.squeezeclient.extfuncs.loadArtwork
 import de.maniac103.squeezeclient.extfuncs.requireParentAs
 import de.maniac103.squeezeclient.extfuncs.serverMightCacheResults
+import de.maniac103.squeezeclient.extfuncs.withRoundedCorners
 import de.maniac103.squeezeclient.model.ArtworkItem
 import de.maniac103.squeezeclient.model.JiveAction
 import de.maniac103.squeezeclient.model.JiveActions
@@ -487,7 +488,7 @@ class MainContentContainerFragment :
                             if (context.imageCacheContains(icon) != true) {
                                 emit(null)
                             }
-                            emit(context.loadArtwork(icon, iconSize))
+                            emit(context.loadArtwork(icon, iconSize)?.withRoundedCorners(context))
                         }
                     }
                     f.titleFlow.combine(iconFlow) { t, i -> tag to PageTitleInfo(t, i) }

app/src/main/java/de/maniac103/squeezeclient/ui/nowplaying/NowPlayingArtworkImageView.kt
@@ -1,0 +1,35 @@
+/*
+ * This file is part of Squeeze Client, an Android client for the LMS music server.
+ * Copyright (c) 2026 Danny Baumann
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package de.maniac103.squeezeclient.ui.nowplaying
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.RelativeCornerSize
+
+class NowPlayingArtworkImageView(context: Context, attrs: AttributeSet) :
+    ShapeableImageView(context, attrs) {
+
+    @Suppress("unused") // Used from motion scene
+    fun setRelativeCornerRadius(amount: Float) {
+        val currentShape = shapeAppearanceModel
+        shapeAppearanceModel = currentShape.toBuilder()
+            .setAllCornerSizes(RelativeCornerSize(amount))
+            .build()
+    }
+}

app/src/main/java/de/maniac103/squeezeclient/ui/slideshow/GalleryFragment.kt
@@ -22,6 +22,7 @@ import android.app.ActivityOptions
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import de.maniac103.squeezeclient.databinding.FragmentGenericListBinding
@@ -74,7 +75,15 @@ class GalleryFragment :
                 }
                 context.startActivity(intent, options?.toBundle())
             }
-            holder.binding.image.loadSlideshowImage(item, true)
+
+            holder.binding.loadingIndicator.isVisible = true
+            holder.binding.image.loadSlideshowImage(item) {
+                listener(
+                    onSuccess = { request, result ->
+                        holder.binding.loadingIndicator.isVisible = false
+                    }
+                )
+            }
             holder.binding.text.text = item.caption
         }
     }

app/src/main/java/de/maniac103/squeezeclient/ui/slideshow/ImageViewActivity.kt
@@ -53,7 +53,7 @@ class ImageViewActivity : AppCompatActivity() {
         binding = ActivityImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.image.loadSlideshowImage(item, false) {
+        binding.image.loadSlideshowImage(item) {
             lifecycle(lifecycle)
             precision(Precision.INEXACT)
             crossfade(true)

app/src/main/java/de/maniac103/squeezeclient/ui/widget/AspectAdjustingShapeableImageView.kt
@@ -1,0 +1,42 @@
+/*
+ * This file is part of Squeeze Client, an Android client for the LMS music server.
+ * Copyright (c) 2026 Danny Baumann
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package de.maniac103.squeezeclient.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.imageview.ShapeableImageView
+
+open class AspectAdjustingShapeableImageView(context: Context, attrs: AttributeSet) :
+    ShapeableImageView(context, attrs) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val drawableWidth = drawable?.intrinsicWidth
+        val drawableHeight = drawable?.intrinsicHeight
+        if (adjustViewBounds && drawableWidth != null && drawableHeight != null) {
+            if (drawableWidth < drawableHeight) {
+                // width becomes dynamic
+                super.onMeasure(MeasureSpec.UNSPECIFIED, heightMeasureSpec)
+            } else {
+                // height becomes dynamic
+                super.onMeasure(widthMeasureSpec, MeasureSpec.UNSPECIFIED)
+            }
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        }
+    }
+}

commit cd393496d889f12847bd26d4247f17a65bd5f85e

Properly hide icons in text-only lists again
This broke in commit e51db9a611de37345cf34f095d14db33ea8717e6

app/src/main/java/de/maniac103/squeezeclient/ui/common/SlimBrowseItemListAdapter.kt
@@ -51,6 +51,7 @@ class SlimBrowseItemListAdapter(
             val original = GridItemSlimbrowseBinding.inflate(inflater, parent, false)
             SlimBrowseItemListViewHolder.ItemBinding(
                 original.root,
+                original.iconWrapper,
                 original.icon,
                 original.title,
                 original.subtext,
@@ -61,6 +62,7 @@ class SlimBrowseItemListAdapter(
             val original = ListItemSlimbrowseBinding.inflate(inflater, parent, false)
             SlimBrowseItemListViewHolder.ItemBinding(
                 original.root,
+                original.iconWrapper,
                 original.icon,
                 original.title,
                 original.subtext,

app/src/main/java/de/maniac103/squeezeclient/ui/common/SlimBrowseItemListViewHolder.kt
@@ -47,7 +47,7 @@ class SlimBrowseItemListViewHolder(
         binding.title.text = item.title
         binding.subText.text = subText
         binding.subText.isVisible = !subText.isNullOrEmpty()
-        binding.icon.isVisible = showIcon
+        binding.iconWrapper.isVisible = showIcon
         if (showIcon) {
             binding.icon.loadArtworkOrPlaceholder(item)
         }
@@ -69,6 +69,7 @@ class SlimBrowseItemListViewHolder(
 
     data class ItemBinding(
         val root: View,
+        val iconWrapper: View,
         val icon: ShapeableImageView,
         val title: TextView,
         val subText: TextView,